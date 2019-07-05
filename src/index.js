bdaa63eb641ce87921165c6c43f607bf466bf402
Ilya Babich <Ilya_Babich@epam.com>
2019-07-05 12:30:19 +0300

    try messaging

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const pintaBot = new PintaBot(token, groupId, confirmation);
 
 pintaBot.setupBot();
 pintaBot.runBot();
-pintaBot.sendMessageToUser("bilyha", "Hello Ilya!");
+pintaBot.sendMessageToUser(102036098, "Hello Ilya!");
 
 router.get("/", async ctx => {
   ctx.set("Cache-Control", "no-cache");
